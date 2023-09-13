2968b9b25644e9aa00fe27ec14a8ba4aec8d6488
Joey van der Poel <Joey.van.der.poel@gmail.com>
2023-09-13 23:43:53 +0200

    3.2.1.C Gemaakt

---
+++ auc-vue/src/components/cabins/Overview31.vue
@@ -0,0 +1,26 @@
+<template>
+
+</template>
+
+<script>
+
+export default {
+  name: "Overview31",
+  data() {
+    return {
+      cabins: [],
+      lastId: 10000,
+    };
+  },
+  created() {
+    for (let i = 0; i < 8; i++) {
+      this.cabins.push(
+          Cabin.createSampleCabin(this.lastId + Math.floor(Math.random() * 3) + 1));
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
