c4b158046de829947c8c562c0e84bb1bf02ae92c
Joey van der Poel <Joey.van.der.poel@gmail.com>
2023-11-26 21:44:06 +0100

    4.1.1 D done

--- backend/src/main/java/app/BackendApplication.java
+++ backend/src/main/java/app/BackendApplication.java
@@ -1,13 +1,47 @@
 package app;
 
+import app.models.Cabin;
+import app.repositories.CabinsRepositoryJpa;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
 @RestController
-public class BackendApplication {
+public class BackendApplication implements CommandLineRunner {
+    private final CabinsRepositoryJpa cabinsRepository;
+
+    public BackendApplication(CabinsRepositoryJpa cabinsRepository) {
+        this.cabinsRepository = cabinsRepository;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
+    }
+
+    @Override
+    @Transactional
+    public void run(String... args) throws Exception {
+        System.out.println("Running CommandLine Startup");
+        this.createInitialCabins();
+    }
+
+    public void createInitialCabins(){
+        List<Cabin> cabins = cabinsRepository.findAll();
+        int createThisManyCabins = 6;
+        if(cabins.size() >= createThisManyCabins) {
+            return; // If there are already 6 or more cabins, exit the method
+        }
+
+        System.out.println("Configure some initial cabins");
+
+        for (int i = 0; i < createThisManyCabins; i++) {
+            Cabin cabin = Cabin.generateRandomCabin(i); // Use 'i' or any unique identifier for cabin creation
+            this.cabinsRepository.save(cabin);
+        }
     }
 }
